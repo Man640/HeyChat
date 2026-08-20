2928abe06613e72fc08a0ae5b3cea4c4194b75fc
Man640 <manu.ap.dev@gmail.com>
2026-08-20 22:38:00 +0530

    initial commit

---
+++ Backend/src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const dotenv = require('dotenv').config({path:"../.env"});
+
+const app = express();
+const PORT = process.env.PORT;
+
+
+app.listen(PORT,()=>{
+    console.log("server is running on Port",PORT);
+})
